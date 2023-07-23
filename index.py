12477487784bdb8afda652b40effd9829ccda4b3
Jiang Chengjun <107529938+lemonorangeapple@users.noreply.github.com>
2023-07-23 12:57:15 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,31 @@
+import urllib.request
+from lxml import etree
+from flask import *
+
+def find(title):
+    result = []
+    page = urllib.request.urlopen("http://libgen.rocks/index.php?req=" + title).read()
+    tree = etree.HTML(page)
+    title = tree.xpath('//*[@id="tablelibgen"]/tbody/tr/td/b/text()')
+    dl = tree.xpath('//*[@id="tablelibgen"]/tbody/tr/td/nobr/a[1]/@href')
+    ext = tree.xpath('//*[@id="tablelibgen"]/tbody/tr/td[8]/text()')
+    for i in range(min(len(dl), len(title), len(ext))):
+        dl[i] = dl[i].replace('http://', 'https://')
+        dl[i] = dl[i].replace('https://libgen.rocks/ads', '')
+        dl[i] = dl[i].replace('https://libgen.me/book/', '')
+        dl[i] = dl[i].replace('https://library.lol/main/', '')
+        dl[i] = dl[i][0:32]
+        if dl[i][0] != '/' and ({'title': title[i], 'dl': 'https://library.lol/main/' + dl[i], 'ext': ext[i]} not in result):
+            result.append({'title': title[i], 'dl': 'https://library.lol/main/' + dl[i], 'ext': ext[i]})
+    return result
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        return render_template('search.html', result = find(request.values.get('q')), q = request.values.get('q'))
+
+app.run()
