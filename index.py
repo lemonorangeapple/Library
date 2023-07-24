1c52f8a857b22cefa74750d3f9afa937429cfd55
Jiang Chengjun <107529938+lemonorangeapple@users.noreply.github.com>
2023-07-24 14:39:03 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -4,19 +4,23 @@ from flask import *
 
 def find(title):
     result = []
-    page = requests.get("http://libgen.top/index.php?req=" + title).text
-    tree = etree.HTML(page)
-    title = tree.xpath('//*[@id="tablelibgen"]/tbody/tr/td/b/text()')
-    dl = tree.xpath('//*[@id="tablelibgen"]/tbody/tr/td/nobr/a[1]/@href')
-    ext = tree.xpath('//*[@id="tablelibgen"]/tbody/tr/td[8]/text()')
-    for i in range(min(len(dl), len(title), len(ext))):
-        dl[i] = dl[i].replace('http://', 'https://')
-        dl[i] = dl[i].replace('https://libgen.rocks/ads', '')
-        dl[i] = dl[i].replace('https://libgen.me/book/', '')
-        dl[i] = dl[i].replace('https://library.lol/main/', '')
-        dl[i] = dl[i][0:32]
-        if dl[i][0] != '/' and ({'title': title[i], 'dl': 'https://library.lol/main/' + dl[i], 'ext': ext[i]} not in result):
-            result.append({'title': title[i], 'dl': 'https://library.lol/main/' + dl[i], 'ext': ext[i]})
+    for j in range(1, 10):
+        try:
+            page = requests.get("http://libgen.top/index.php?req=" + str(title) + "&page=" + j).text
+        except:
+            page = requests.get("http://libgen.top/index.php?req=" + str(title)).text
+        tree = etree.HTML(page)
+        title = tree.xpath('//*[@id="tablelibgen"]/tbody/tr/td/b/text()')
+        dl = tree.xpath('//*[@id="tablelibgen"]/tbody/tr/td/nobr/a[1]/@href')
+        ext = tree.xpath('//*[@id="tablelibgen"]/tbody/tr/td[8]/text()')
+        for i in range(min(len(dl), len(title), len(ext))):
+            dl[i] = dl[i].replace('http://', 'https://')
+            dl[i] = dl[i].replace('https://libgen.rocks/ads', '')
+            dl[i] = dl[i].replace('https://libgen.me/book/', '')
+            dl[i] = dl[i].replace('https://library.lol/main/', '')
+            dl[i] = dl[i][0:32]
+            if dl[i][0] != '/' and ({'title': title[i], 'dl': 'https://library.lol/main/' + dl[i], 'ext': ext[i]} not in result):
+                result.append({'title': title[i], 'dl': 'https://library.lol/main/' + dl[i], 'ext': ext[i]})
     return result
 
 app = Flask(__name__)
@@ -28,3 +32,4 @@ def index():
     elif request.method == 'POST':
         return render_template('search.html', result = find(request.values.get('q')), q = request.values.get('q'))
 
+app.run()
